73edf8a67dd5e6b553013e928fd5373aaa3a0e68
dhlee <dh.lee@xiilab.com>
2025-11-19 11:11:27 +0900

    Revert "Implement setting page components and modals for user management"
    This reverts commit 3545b2a874a2f48207f3cdd1a5a6f4dc35c16866.

--- src/app/(mode)/standard/setting/page.tsx
+++ src/app/(mode)/standard/setting/page.tsx
@@ -1,11 +1,9 @@
 import type { Metadata } from "next";
 
-import SettingMain from "@/components/setting/setting-main";
-
 export const metadata: Metadata = {
   title: "Setting",
 };
 
 export default function StandardSettingPage() {
-  return <SettingMain />;
+  return null;
 }
--- src/components/common/slider/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default } from "./custom-slider";
-export { default as Slider } from "./custom-slider";
-export type { SliderProps, SliderColorType } from "./types";
-export { SLIDER_COLOR_CONFIG, DEFAULT_SLIDER_CONFIG } from "./types";
