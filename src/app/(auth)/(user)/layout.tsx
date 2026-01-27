27b319d8c2b8a92accc234e98ff067a7d834f6dd
dhlee <dh.lee@xiilab.com>
2026-01-27 19:21:55 +0900

    fix: auth 레이아웃 수정

--- src/app/(auth)/(user)/layout.tsx
+++ src/app/(auth)/(user)/layout.tsx
@@ -7,6 +7,7 @@ import { Typography } from "xiilab-ui";
 
 import { LoginBackgroundIcon } from "@/shared/components/icon/login-background";
 import { LEGAL_INFO } from "@/shared/constants/legal.constant";
+import { customScrollbar } from "@/styles/mixins/scrollbar";
 
 /**
  * 사용자 인증 관련 페이지 공통 레이아웃
@@ -15,33 +16,35 @@ import { LEGAL_INFO } from "@/shared/constants/legal.constant";
 export default function UserAuthLayout({ children }: PropsWithChildren) {
   return (
     <Container>
-      <Left>
-        <LeftBody>{children}</LeftBody>
-        <LeftFooter>
-          <FooterLinks>
-            {LEGAL_INFO.FOOTER_LINKS.map((link, index) => (
-              <span key={link.label}>
-                <StyledLink href={link.href}>
-                  <Typography.Text variant="body-4-2" color="#828588">
-                    {link.label}
-                  </Typography.Text>
-                </StyledLink>
-                {index < LEGAL_INFO.FOOTER_LINKS.length - 1 && (
-                  <Typography.Text variant="body-4-2" color="#828588">
-                    ｜
-                  </Typography.Text>
-                )}
-              </span>
-            ))}
-          </FooterLinks>
-          <Typography.Text variant="body-4-2" color="#828588">
-            {LEGAL_INFO.COMPANY_INFO}
-          </Typography.Text>
-        </LeftFooter>
-      </Left>
-      <Right>
-        <LoginBackgroundIcon />
-      </Right>
+      <ContainerInner>
+        <Left>
+          <LeftBody>{children}</LeftBody>
+          <LeftFooter>
+            <FooterLinks>
+              {LEGAL_INFO.FOOTER_LINKS.map((link, index) => (
+                <span key={link.label}>
+                  <StyledLink href={link.href}>
+                    <Typography.Text variant="body-4-2" color="#828588">
+                      {link.label}
+                    </Typography.Text>
+                  </StyledLink>
+                  {index < LEGAL_INFO.FOOTER_LINKS.length - 1 && (
+                    <Typography.Text variant="body-4-2" color="#828588">
+                      ｜
+                    </Typography.Text>
+                  )}
+                </span>
+              ))}
+            </FooterLinks>
+            <Typography.Text variant="body-4-2" color="#828588">
+              {LEGAL_INFO.COMPANY_INFO}
+            </Typography.Text>
+          </LeftFooter>
+        </Left>
+        <Right>
+          <LoginBackgroundIcon />
+        </Right>
+      </ContainerInner>
     </Container>
   );
 }
@@ -49,17 +52,23 @@ export default function UserAuthLayout({ children }: PropsWithChildren) {
 const Container = styled.div`
   width: 100vw;
   height: 100vh;
+  overflow-x: auto;
+  overflow-y: hidden;
+  background-color: #fff;
+
+  ${customScrollbar()}
+`;
+
+const ContainerInner = styled.div`
+  height: 100%;
   display: flex;
   justify-content: flex-start;
   align-items: center;
-  background-color: #fff;
-  overflow: hidden;
 `;
 
 const Left = styled.div`
-  flex-basis: 620px;
+  width: 620px;
   flex-shrink: 0;
-  flex-grow: 0;
   height: 100%;
   padding: 30px 90px;
   display: flex;
@@ -67,10 +76,10 @@ const Left = styled.div`
 `;
 
 const Right = styled.div`
-  position: relative;
-  height: 100%;
   flex: 1;
-  min-width: 1300px;
+  min-width: 620px;
+  height: 100%;
+  position: relative;
 `;
 
 const LeftBody = styled.div`
